0879ab9dc6bb2b2009baf19668225419cdc7fddc
Ramiro Mineldin <68164810+ramiromineldin@users.noreply.github.com>
2021-02-02 17:20:16 -0300

    Update zyxcba.c

--- zyxcba.c
+++ zyxcba.c
@@ -8,6 +8,7 @@
 #define COMANDO_ATENDER "ATENDER_SIGUIENTE"
 #define COMANDO_INFORME "INFORME"
 
+
 bool validar_cantidad_parametros (char** parametros, size_t cant) {
 	size_t contados = 0;
 	while (parametros[contados]) {
@@ -16,14 +17,15 @@ bool validar_cantidad_parametros (char** parametros, size_t cant) {
 	return contados = cant;
 }
 
-void procesar_comando(const char* comando, const char** parametros) {
+void procesar_comando(const char* comando, const char** parametros, abb_t *doctores, hash_t *pacientes, hash_t *especialidades) {
 	if (strcmp(comando, COMANDO_PEDIR_TURNO) == 0) {
 		if (!validar_cantidad_parametros(parametros, 3)) printf(ENOENT_PARAMS, comando);
+		else pedir_turno(pacientes, especialidades, parametros);
 
 	} 
 	else if (strcmp(comando, COMANDO_ATENDER) == 0) {
 		if (!validar_cantidad_parametros(parametros, 1)) printf(ENOENT_PARAMS, comando);
-
+		else atender_siguiente_paciente(doctores, pacientes, especialidades, parametros);
 
 	} 
 	else if (strcmp(comando, COMANDO_INFORME) == 0) {
@@ -32,7 +34,7 @@ void procesar_comando(const char* comando, const char** parametros) {
 
 	} 
 	else {
-
+		printf(ENOENT_CMD, comando);
 	}
 }
 
@@ -43,7 +45,7 @@ void eliminar_fin_linea(char* linea) {
 	}
 }
 
-void procesar_entrada() {
+void procesar_entrada(abb_t *doctores, hash_t *pacientes, hash_t *especialidades) {
 	char* linea = NULL;
 	size_t c = 0;
 	while (getline(&linea, &c, stdin) > 0) {
@@ -69,6 +71,18 @@ int main(int argc, char** argv) {
 		printf(ENOENT_CANT_PARAMS);
 		return 1;
 	}
+	hash_t *especialidades = hash_crear(destruir_especialidad);
+	if (!especialidades) return 1;
+	abb_t *doctores = guardar_doctores(argv[0], especialidades, cmp_anio_ingreso);
+	if (!doctores) return 1
+	hash_t *pacientes = guardar_pacientes(argv[1]);
+	if (!pacientes) return 1;
 
+	procesar_entrada(docores, pacientes, especialidades);
+
+	hash_destruir(especialidades);
+	abb_destruir(doctores);
+	hash_destruir(pacientes);
+	
 	return 0;
 }
