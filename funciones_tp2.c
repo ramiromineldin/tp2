d67de041ff18aff8635742d5b8ddff5db881086b
nathencinoza <71742037+nathencinoza@users.noreply.github.com>
2021-02-19 10:57:18 -0300

    Update funciones_tp2.c

--- funciones_tp2.c
+++ funciones_tp2.c
@@ -1,10 +1,29 @@
 #include "funciones_tp2.h"
+#include "struct_tp2.h"
+#include <string.h>
 
 #define REGULAR "REGULAR"
 #define URGENTE "URGENTE"
+struct paciente {
+    char *nombre;
+    int *anio;
+};
+
+struct doctor {
+    char *nombre;
+    char *especialidad;
+    size_t *atendidos;
+};
+
+struct especialidad {
+    char *nombre;
+    cola_t *urgentes;
+    heap_t *regulares;
+    size_t en_espera;
+};
 
 
-void pedir_turno(hash_t *pacientes, hash_t *especialidades, const char **parametros) {
+void pedir_turno(hash_t *pacientes, hash_t *especialidades, char **parametros) {
     if (!hash_pertenece(pacientes, parametros[0])) printf(ENOENT_PACIENTE, parametros[0]);
     
     else if (!hash_pertenece(especialidades, parametros[1])) printf(ENOENT_ESPECIALIDAD, parametros[1]);
@@ -20,18 +39,18 @@ void pedir_turno(hash_t *pacientes, hash_t *especialidades, const char **paramet
             cola_encolar(especialidad->urgentes, paciente);
         }
         especialidad->en_espera++;
-        printf(PACIENTE_ENCOLADO, parametros[0]);
-        printf(CANT_PACIENTES_ENCOLADOS, espcialidad->en_espera);
+        printf(PACIENTE_ENCOLADO, paciente->nombre);
+        printf(CANT_PACIENTES_ENCOLADOS,*(size_t*) especialidad->en_espera, especialidad->nombre);
     }
 }
 
-void atender_siguiente_paciente(abb_t *doctores, hash_t *pacientes, hash_t *especialidades, const char **parametros) {
+void atender_siguiente_paciente(abb_t *doctores, hash_t *pacientes, hash_t *especialidades, char **parametros) {
     if (!abb_pertenece(doctores, parametros[0])) printf(ENOENT_DOCTOR, parametros[0]);
 
     doctor_t *doctor = abb_obtener(doctores, parametros[0]);
     especialidad_t *especialidad = hash_obtener(especialidades, doctor->especialidad);
     if (especialidad->en_espera == 0) printf(SIN_PACIENTES);
-    paciente_t *paciente
+    paciente_t *paciente;
     if (!cola_esta_vacia(especialidad->urgentes)) {
         paciente = cola_desencolar(especialidad->urgentes);
     }
@@ -41,7 +60,7 @@ void atender_siguiente_paciente(abb_t *doctores, hash_t *pacientes, hash_t *espe
     
     printf(PACIENTE_ATENDIDO, paciente->nombre);
     especialidad->en_espera--;
-    printf(CANT_PACIENTES_ENCOLADOS, dcotor->especialidad);    
+    printf(CANT_PACIENTES_ENCOLADOS, especialidad->en_espera, doctor->especialidad);    
     doctor->atendidos++;
     hash_borrar(pacientes, paciente->nombre);
     destruir_paciente(paciente);
@@ -50,17 +69,18 @@ void atender_siguiente_paciente(abb_t *doctores, hash_t *pacientes, hash_t *espe
 
 /* visitar */
 bool imprimir_informes(const char *nombre_doctor, void *doctor, void *extra, size_t *cant) {
-    doctor_t *doctor = doctor;
+    doctor_t *doctor_aux = doctor;
     if (strcmp(nombre_doctor, extra) != 0) {
-        *cant++;
-        printf(INFORME_DOCTOR,*cant, nombre_doctor, doctor->especialidad, doctor->atendidos)
-        return true
+        *cant += 1;
+        printf(INFORME_DOCTOR,*(size_t*)cant, nombre_doctor, doctor_aux->especialidad,*(size_t*)doctor_aux->atendidos);
+        return true;
     }
-    return false
+    return false;
 }
 
-void guardar_informes(abb_t *doctores, const char **parametros) {
+void guardar_informes(abb_t *doctores, char **parametros) {
     printf(DOCTORES_SISTEMA, abb_cantidad(doctores));
-    size_t cant = 0
+    size_t cant = 0;
     abb_in_order(doctores, imprimir_informes, parametros[0], parametros[1], &cant);
 }
+
