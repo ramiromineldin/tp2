3fea7b9e4790ae724c8ffa2f30762ed51a229dca
nathencinoza <71742037+nathencinoza@users.noreply.github.com>
2021-02-20 15:02:52 -0300

    Update funciones_tp2.h

--- funciones_tp2.h
+++ funciones_tp2.h
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "mensajes.h"
+#include "struct_tp2.h"
 #include <stdlib.h>
 
 
 /* Recibe un hash con los pacientes, un hash con las especialidades y los parametros del comando y pide un turno.
    En caso de error imprime un mensaje */
-void pedir_turno(hash_t *pacientes, hash_t *especialidades, const char **parametros);
+void pedir_turno(hash_t *pacientes, hash_t *especialidades,char **parametros);
 
 /* Recibe un abb con los doctores, un hash con los pacientes, un hash con las especialidades y los parametrso del comando 
    y atiende al siguiente paciente.
    En caso de error imprime un mensaje */
-void atender_siguiente_paciente(abb_t *doctores, hash_t *pacientes, hash_t *especialidades, const char **parametros);
+void atender_siguiente_paciente(abb_t *doctores, hash_t *pacientes, hash_t *especialidades,  char **parametros);
 
 bool imprimir_informes(const char *nombre_doctor, void *doctor, void *extra, size_t *cant);
-void guardar_informes(abb_t *doctores, const char **parametros);
+void guardar_informes(abb_t *doctores, char **parametros);
