c748bbb7eb6f33b2c67a27fd02f4151c36adbb63
nathencinoza <71742037+nathencinoza@users.noreply.github.com>
2021-02-22 15:19:13 -0300

    Update zyxcba.c

--- zyxcba.c
+++ zyxcba.c
@@ -1,108 +1,91 @@
-#include <stdio.h>
-#include <string.h>
 #include "funciones_tp2.h"
-#include "strutil.h"
-#include "mensajes.h"
+#include "struct_tp2.h"
+#include <string.h>
 
-#define COMANDO_PEDIR_TURNO "PEDIR_TURNO"
-#define COMANDO_ATENDER "ATENDER_SIGUIENTE"
-#define COMANDO_INFORME "INFORME"
+#define REGULAR "REGULAR"
+#define URGENTE "URGENTE"
+struct paciente {
+    char *nombre;
+    int *anio;
+};
+
+struct doctor {
+    char *nombre;
+    char *especialidad;
+    size_t *atendidos;
+};
+
+struct especialidad {
+    char *nombre;
+    cola_t *urgentes;
+    heap_t *regulares;
+    size_t en_espera;
+};
 
 
-bool validar_cantidad_parametros (char** parametros, size_t cant) {
-	size_t contados = 0;
-	while (parametros[contados]) {
-		contados++;
-	}
-	return (contados == cant);
+void pedir_turno(hash_t *pacientes, hash_t *especialidades, char **parametros) {
+    if (!hash_pertenece(pacientes, parametros[0])) printf(ENOENT_PACIENTE, parametros[0]);
+    
+    else if (!hash_pertenece(especialidades, parametros[1])) printf(ENOENT_ESPECIALIDAD, parametros[1]);
+
+    else if (strcmp(parametros[2], REGULAR) != 0 && strcmp(parametros[2], URGENTE) != 0) printf(ENOENT_URGENCIA, parametros[2]);
+
+    else {
+        especialidad_t *especialidad = hash_obtener(especialidades, parametros[1]);
+        paciente_t *paciente = hash_obtener(pacientes, parametros[0]);
+        if (strcmp(parametros[2], REGULAR) == 0) heap_encolar(especialidad->regulares, paciente);
+        
+        else {
+            cola_encolar(especialidad->urgentes, paciente);
+        }
+        especialidad->en_espera++;
+        printf(PACIENTE_ENCOLADO, paciente->nombre);
+        printf(CANT_PACIENTES_ENCOLADOS,*(size_t*) especialidad->en_espera, especialidad->nombre);
+    }
 }
 
-void procesar_comando(const char* comando, char** parametros, abb_t *doctores, hash_t *pacientes, hash_t *especialidades) {
-	
-	if (strcmp(comando, COMANDO_PEDIR_TURNO) == 0) {
-		if (!validar_cantidad_parametros(parametros, 3)) printf(ENOENT_PARAMS, comando);
-		else pedir_turno(pacientes, especialidades, parametros);
+void atender_siguiente_paciente(abb_t *doctores, hash_t *pacientes, hash_t *especialidades, char **parametros) {
+    if (!abb_pertenece(doctores, parametros[0])) {
+        printf(ENOENT_DOCTOR, parametros[0]);
+        return;
+    }
 
-	} 
-	else if (strcmp(comando, COMANDO_ATENDER) == 0) {
-		if (!validar_cantidad_parametros(parametros, 1)) printf(ENOENT_PARAMS, comando);
-		else atender_siguiente_paciente(doctores, pacientes, especialidades, parametros);
-
-	} 
-	else if (strcmp(comando, COMANDO_INFORME) == 0) {
-		if (!validar_cantidad_parametros(parametros, 2)) printf(ENOENT_PARAMS, comando);
-		else guardar_informes(doctores, parametros);
-
-	} 
-	else {
-		printf(ENOENT_CMD, comando);
-	}
+    doctor_t *doctor = abb_obtener(doctores, parametros[0]);
+    especialidad_t *especialidad = hash_obtener(especialidades, doctor->especialidad);
+    if (especialidad->en_espera == 0) printf(SIN_PACIENTES);
+    paciente_t *paciente = NULL;
+    if (!cola_esta_vacia(especialidad->urgentes)) {
+        paciente = cola_desencolar(especialidad->urgentes);
+    }
+    else if (!heap_esta_vacio(especialidad->regulares)) {
+        paciente = heap_desencolar(especialidad->regulares);
+    }
+    if (paciente) {
+        printf(PACIENTE_ATENDIDO, paciente->nombre);
+        especialidad->en_espera--;
+        printf(CANT_PACIENTES_ENCOLADOS, especialidad->en_espera, doctor->especialidad);    
+        doctor->atendidos++;
+        hash_borrar(pacientes, paciente->nombre);
+        destruir_paciente(paciente);
+    }
 }
 
-void eliminar_fin_linea(char* linea) {
-	size_t len = strlen(linea);
-	if (linea[len - 1] == '\n') {
-		linea[len - 1] = '\0';
-	}
+/* visitar */
+bool imprimir_informes(const char *nombre_doctor, void *doctor, void *extra, size_t *cant) {
+    doctor_t *doctor_aux = doctor;
+    if (strcmp(nombre_doctor, extra) != 0) {
+        *cant += 1;
+        printf(INFORME_DOCTOR,*(size_t*)cant, nombre_doctor, doctor_aux->especialidad,*(size_t*)doctor_aux->atendidos);
+        return true;
+    }
+    return false;
 }
 
-void procesar_entrada(abb_t *doctores, hash_t *pacientes, hash_t *especialidades) {
-	char* linea = NULL;
-	size_t c = 0;
-	while (getline(&linea, &c, stdin) > 0) {
-		eliminar_fin_linea(linea);
-		char** campos = split(linea, ':');
-		if (campos[1] == NULL) {
-			printf(ENOENT_FORMATO, linea);
-			free_strv(campos);
-			continue;	
-		}
-		char** parametros = split(campos[1], ',');
-		procesar_comando(campos[0], parametros, doctores, pacientes, especialidades);
-		free_strv(parametros);
-		free_strv(campos);
-	}
-	free(linea);
+void guardar_informes(abb_t *doctores, char **parametros) {
+    printf(DOCTORES_SISTEMA, abb_cantidad(doctores));
+    size_t cant = 0;
+    if (abb_cantidad(doctores) == 0) {
+        return;
+    }
+    abb_in_order(doctores, imprimir_informes, parametros[0], parametros[1], &cant);
 }
-
-int cmp (const void* a, const void* b) {
-	int *izq = (int*)a; 
-	int *der = (int*)b;
-	if (*izq < *der) {
-		return -1; 
-	}
-	else if (*izq > *der) {
-		return 1; 
-	}
-	return 0; 
-}
-
-int main(int argc, char** argv) {
-	if (argc != 3) {
-		printf(ENOENT_CANT_PARAMS);
-		return 1;
-	}
-	hash_t *especialidades = hash_crear(destruir_especialidad);
-	if (!especialidades) return 1;
-	
-	abb_t *doctores = guardar_doctores(argv[1], especialidades, cmp);
-	if (!doctores) {
-		hash_destruir(especialidades);
-		return 1;
-	} 
-	
-	hash_t *pacientes = guardar_pacientes(argv[2]);
-	if (!pacientes) {
-		hash_destruir(especialidades);
-		abb_destruir(doctores);
-		return 1;
-	}
-	procesar_entrada(doctores, pacientes, especialidades);
-
-	hash_destruir(especialidades);
-	abb_destruir(doctores);
-	hash_destruir(pacientes);
-	
-	return 0;
-}
-
